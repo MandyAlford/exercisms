2baa319da9eedc3c906dd94f99d451f587418a69
Mandy Alford <mandyalford@gmail.com>
2020-06-30 14:08:56 -0600

    submit

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -2,6 +2,16 @@
 // This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// - Black: 0
+// - Brown: 1
+// - Red: 2
+// - Orange: 3
+// - Yellow: 4
+// - Green: 5
+// - Blue: 6
+// - Violet: 7
+// - Grey: 8
+// - White: 9
 
 export const colorCode = (color) => {
   return COLORS.indexOf(color)
