c09a2f9f3ccce7b9bffd7416753eebdc24e0af1e
Mandy Alford <mandyalford@gmail.com>
2020-07-08 12:19:22 -0600

    Create protein object

---
+++ protein-translation/protein-translation.js
@@ -0,0 +1,29 @@
+//
+// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const translate = () => {
+  
+  // throw new Error("Remove this statement and implement this function");
+};
+
+let proteins = {
+  AUG:	Methionine,
+  UUU: Phenylalanine,
+  UUC: Phenylalanine,
+  UUA: Leucine,
+  UUG: Leucine,
+  UCU: Serine,
+  UCC: Serine,
+  UCA: Serine,
+  UCG: Serine,
+  UAU: Tyrosine,
+  UAC: Tyrosine,
+  UGU: Cysteine,
+  UGC: Cysteine,
+  UGG:	Tryptophan,
+  UAA: STOP,
+  UAG: STOP,
+  UGA: STOP,
+}
