9aa98e256353db44e316cec70c0bba22b8b23c78
Mandy Alford <mandyalford@gmail.com>
2020-07-13 11:59:10 -0600

    pass test 1

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -6,6 +6,22 @@
 // 9 = 9^1 = 9
 // 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
 
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
+// split number into digits.
+// take each number and multiply it by the number of digits (length of array?)
+// if those sums equal the original number, return true
+
+export const isArmstrongNumber = (num) => {
+  let numArray = num.toString().split('')
+  let sums = [];
+
+  for(var i = 0; i < numArray.length; i++) {
+    let test = parseInt(Math.pow(numArray[i], numArray.length))
+    sums.push(test)
+  }
+  let answer = sums.reduce(function(a, b){
+      return a + b;
+  }, 0);
+  if (answer === num) {
+    return true
+  }
 };
