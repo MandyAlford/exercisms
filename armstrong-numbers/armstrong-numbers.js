e7da1be9dae32d7e8b2c46ab9d06a1b2c7e0af68
Mandy Alford <mandyalford@gmail.com>
2020-07-13 11:30:58 -0600

    wrrite init pseudocode

---
+++ armstrong-numbers/armstrong-numbers.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+// An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+// 9 = 9^1 = 9
+// 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+export const isArmstrongNumber = () => {
+  throw new Error("Remove this statement and implement this function");
+};
