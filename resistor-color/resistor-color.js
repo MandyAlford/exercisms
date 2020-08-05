2e944d201b0c19218745ffe0160976fe566590ce
Mandy Alford <mandyalford@gmail.com>
2020-08-05 10:40:16 -0400

    download gigasecond and dependencies

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,7 +1,4 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+
 // - Black: 0
 // - Brown: 1
 // - Red: 2
