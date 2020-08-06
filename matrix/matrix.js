7077d8c34c3be458945ec5b09eff7176c4f93dda
Mandy Alford <mandyalford@gmail.com>
2020-08-06 12:28:13 -0400

    add get rows func

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,15 +1,28 @@
 //
 // This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
+//.
 
 export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(data) {
+    this.data = data;
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+    let splitData = this.data.split('\n');
+    let parsedData = [];
+    // console.log(splitData)
+
+    // numData = array of arrays containing strings
+    let newData = splitData.map(element => {
+       return element.split(' ')
+    })
+    let numData = newData.map(element => {
+      return element.map(string => {
+        return parseInt(string)
+      })
+    })
+    return numData
   }
 
   get columns() {
