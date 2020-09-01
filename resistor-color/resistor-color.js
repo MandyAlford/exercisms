8fed2b53869125613c410575309894413c49206b
Mandy Alford <mandyalford@gmail.com>
2020-09-01 14:54:54 -0400

    submit solution

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -14,4 +14,15 @@ export const colorCode = (color) => {
   return COLORS.indexOf(color)
 };
 
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+]
