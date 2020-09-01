d41b6addc3296e562d0288f8f9f85a9c7b255af2
Mandy Alford <mandyalford@gmail.com>
2020-09-01 12:08:45 -0400

    solve with reduce instead of for loop for practice

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -4,19 +4,27 @@
 //
 
 export const compute = (strand1, strand2) => {
-  let differences = 0
-  // console.log('1', strand1)
-  // console.log('2', strand2)
-
-  // compare character by character of both arrays
-  if ((strand1.length !== strand2.length) && (strand1 === '')){
+  if (strand1 === '' && strand2 === '') {
+    return 0
+  } else if (strand1 === ''){
     throw new Error('left strand must not be empty')
-} else if ((strand1.length !== strand2.length) && (strand2 === '')) {
-  throw new Error('right strand must not be empty')
-} else if(strand1.length !== strand2.length) {
-  throw new Error('left and right strands must be of equal length')
-}
+  } else if (strand2 === '') {
+    throw new Error('right strand must not be empty')
+  } else if(strand1.length !== strand2.length) {
+    throw new Error('left and right strands must be of equal length')
+  }
 
+//  solution with reduce
+  // let splitStrand= strand1.split('')
+
+  // return splitStrand.reduce((acc, letter, index)=> {
+  //   if(strand2[index] !== letter) {
+  //     acc++
+  //   }
+  //   return acc
+  // }, 0)
+
+  let differences = 0
   for(var i=0; i<= strand1.length; i++) {
     if(strand1[i] !== strand2[i]){
       differences += 1
