e762f8622bc37b5ddcffb570564d46df39249fd5
Mandy Alford <mandyalford@gmail.com>
2020-09-01 15:09:12 -0400

    Initial test pass using bracket notation via answer object

---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRna = (nucleotide) => {
+  return nucleotides[nucleotide]
+};
+
+const nucleotides = {
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U',
+  '': ''
+}
