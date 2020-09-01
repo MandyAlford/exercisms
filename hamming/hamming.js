2919c1b5e6108a1302dd89791864be7e9b128d0e
Mandy Alford <mandyalford@gmail.com>
2020-09-01 11:29:53 -0400

    Pass initial tests

---
+++ hamming/hamming.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const compute = (strand1, strand2) => {
+  console.log('1', strand1)
+  console.log('2', strand2)
+
+  if(strand1 === strand2) {
+    return 0
+  }
+};
