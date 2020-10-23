2df32eb5059f30cc3c424f1d65001418de2a074c
Mandy Alford <mandyalford@gmail.com>
2020-10-23 14:29:41 -0400

    clean up code and remove comments

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,14 +1,8 @@
-//
-// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const findAnagrams = (string, choices) => {
   let placeholder = []
   let sortedString = string.toLowerCase().split('').sort().join('')
 
   for(var i = 0; i < choices.length; i++) {
-    // console.log(choices[i].toLowerCase().split('').sort().join(''))
     if (
       (choices[i].toLowerCase().split('').sort().join('') === sortedString) && 
       string.toLowerCase() != choices[i].toLowerCase()
@@ -16,7 +10,5 @@ export const findAnagrams = (string, choices) => {
       placeholder.push(choices[i])
     }
   }
-  // console.log(placeholder)
-  // console.log(sortedString)
   return placeholder
 }
