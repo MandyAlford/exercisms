a898ca5d91a98810e9a52a0c4bbbf3d68e65a791
Mandy Alford <mandyalford@gmail.com>
2020-10-23 13:50:31 -0400

    set up dependencies;

---
+++ anagram/anagram.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const findAnagrams = (string, choices) => {
+  for(var i = 0; i < string.length; i++) {
+    console.log(choice[i])
+  }
+  //  if hit a mismatch, end and move to next word
+  // if choice has all letters, return that chioce
+}
