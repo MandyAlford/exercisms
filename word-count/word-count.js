b5d6edaf1533cef2966d86b5f4c22fc4db76c12f
Mandy Alford <mandyalford@gmail.com>
2020-10-26 13:55:20 -0400

    Update regex to account for new line and punctuation

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,11 +1,20 @@
 //
 // This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
+// /g in regex, both in parens and out
+// /g
 
 export const countWords = (phrase) => {
   let result = {}
-  let phraseArray = phrase.split(' ')
+  // let phraseArray = phrase.replace(/(,\n|, |,)/g, ' ').split(' ')
+  let phraseArray = phrase.replace(/(\W\n|\W+)/g, ' ').trimEnd().toLowerCase().split(' ')
+
+
+
+  // let filteredArray = phraseArray.filter(word => re)
+  // console.log('phrase', phrase)
+  // console.log('newString', newString)
+  console.log('phraseArray', phraseArray)
 
   for(var i = 0; i < phraseArray.length; i++) {
     // console.log(result[phraseArray[i]])
@@ -15,5 +24,6 @@ export const countWords = (phrase) => {
       result[phraseArray[i]] ++
     }
   }
+  console.log('result', result)
   return result;
 };
