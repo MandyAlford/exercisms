48d20d4060660cf9f5a2ba1f9980baa4202c89c4
Mandy Alford <mandyalford@gmail.com>
2020-11-03 11:10:09 -0500

    pass all tests

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,15 +1,3 @@
-
-// - Black: 0
-// - Brown: 1
-// - Red: 2
-// - Orange: 3
-// - Yellow: 4
-// - Green: 5
-// - Blue: 6
-// - Violet: 7
-// - Grey: 8
-// - White: 9
-
 export const colorCode = (color) => {
   return COLORS.indexOf(color)
 };
