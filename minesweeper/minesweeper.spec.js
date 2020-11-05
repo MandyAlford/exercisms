082392e74f3fa82e17b49c0bb89d67584e39c71b
Mandy Alford <mandyalford@gmail.com>
2020-11-05 10:16:26 -0500

    pass initial tests

--- minesweeper/minesweeper.spec.js
+++ minesweeper/minesweeper.spec.js
@@ -9,7 +9,7 @@ describe(')', () => {
     expect(annotate([''])).toEqual(['']);
   });
 
-  xtest('handles no mines', () => {
+  test('handles no mines', () => {
     const input = [
       '   ',
       '   ',
@@ -23,7 +23,7 @@ describe(')', () => {
     expect(annotate(input)).toEqual(expected);
   });
 
-  xtest('handles board with only mines', () => {
+  test('handles board with only mines', () => {
     const input = [
       '***',
       '***',
@@ -37,7 +37,7 @@ describe(')', () => {
     expect(annotate(input)).toEqual(expected);
   });
 
-  xtest('handles mine surrounded by spaces', () => {
+  test('handles mine surrounded by spaces', () => {
     const input = [
       '   ',
       ' * ',
